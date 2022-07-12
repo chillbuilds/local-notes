fb3a4326677e6ca317469bda189383e67bfe3bf2
Will Wojeck <will.wojeck@leafgroup.com>
2022-07-11 18:13:53 -0600

    added note directory check

--- public/scripts/script.js
+++ public/scripts/script.js
@@ -1,3 +1,41 @@
 const fs = require('fs')
+const { BlockList } = require('net')
+const path = require('path')
 
-fs.writeFile('/Users/willwojeck/desktop/test.txt', 'testicles', (data) => {alert('done')})
+var dir = fs.readFileSync(path.join(__dirname,'../assets/text/dir.txt'), 'utf-8')
+
+
+if(dir.split('').length == 0){
+    $('#modal-element').html(`
+        <div style="text-align:center;">
+            please enter note directory<br><br>
+        </div>
+        <input style="width:100%;" id="dir"></input>
+        <img class="icon" id="update" style="position:relative;float:right;" src="../assets/images/check.png">
+        <script>
+        $('#update').on('click', ()=>{
+            let valSplit = $('#dir').val().split('')
+            if(valSplit[valSplit.length-1] == '/'){
+                fs.writeFileSync(path.join(__dirname,'../assets/text/dir.txt'), $('#dir').val())
+            }
+            else{
+                fs.writeFileSync(path.join(__dirname,'../assets/text/dir.txt'), $('#dir').val()+'/')
+            }
+            $('.modal').attr('style', 'display:none;')
+        })
+        </script>
+    `)
+    $('.modal').attr('style', 'display:inline-block;')
+}else{
+    $('#note-dir').val = dir
+    
+}
+
+
+$('#myBtn').on('click', ()=>{
+    $('.modal').attr('style', 'display:inline-block;')
+})
+
+$('.close').on('click', ()=>{
+    $('.modal').attr('style', 'display:none;')
+})
